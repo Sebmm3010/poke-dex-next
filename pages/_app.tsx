f4d22e0fa5f60f832555ddc5dd5ec12e6fe247f0
Sebastian <rockmadero@gmail.com>
2022-12-28 13:15:10 -0500

    darktheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme } from '../themes';
 import { Roboto } from '@next/font/google'
 
-const roboto= Roboto({
+const roboto = Roboto({
   weight: ['300', '500', '700'],
-  subsets:['latin']
+  subsets: ['latin']
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={roboto.className}>
-      <Component {...pageProps} />
+      <NextUIProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </NextUIProvider>
     </main>
   )
 }
