9d540f726e266b2331d652de9f19f6a7910cafea
Sebastian <rockmadero@gmail.com>
2022-12-29 16:10:35 -0500

    get static props

---
+++ components/pokemon/PokeCardItem.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react'
+import { Card, Grid, Row, Text } from '@nextui-org/react'
+import { SmallPokemon } from '../../interfaces'
+
+interface Props{
+    pokemon:SmallPokemon
+}
+
+export const PokeCardItem:FC<Props> = ({pokemon}) => {
+    const {id, name, img}= pokemon;
+  return (
+      <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+          <Card hoverable clickable>
+              <Card.Body>
+                  <Card.Image
+                      src={img}
+                      width="100%"
+                      height={140}
+                  />
+              </Card.Body>
+              <Card.Footer>
+                  <Row justify="space-between">
+                      <Text transform="capitalize">{name}</Text>
+                      <Text>#{id}</Text>
+                  </Row>
+              </Card.Footer>
+          </Card>
+      </Grid>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,52 @@
-import { NextPage } from "next";
-import { Button } from '@nextui-org/react';
+import { Grid } from "@nextui-org/react";
+import { NextPage, GetStaticProps } from "next";
+import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
+import { PokemonList, SmallPokemon } from "../interfaces";
+import { PokeCardItem } from '../components/pokemon/PokeCardItem';
 
-const HomePage: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+
+
   return (
     <Layout title="Pokemon-App">
-      <Button color="gradient">
-        Hola mundo
-      </Button>
+
+      <Grid.Container gap={1} justify='flex-start'>
+        {pokemons.map((pokemon) => (
+          <PokeCardItem key={pokemon.id} pokemon={pokemon}/>
+        ))
+        }
+      </Grid.Container>
+
     </Layout>
   )
+}
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg
+  const { data } = await pokeApi.get<PokemonList>('/pokemon?limit=151');
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => ({
+    ...pokemon,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`
+  }));
+
+
+  return {
+    props: {
+      pokemons
+    }
+  }
 }
 
 export default HomePage;
