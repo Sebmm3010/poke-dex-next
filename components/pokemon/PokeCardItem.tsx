0d852f431c5f283330d7fa7cd77af14e15c72b78
Sebastian <rockmadero@gmail.com>
2022-12-30 12:39:57 -0500

    pokeCard item

--- components/pokemon/PokeCardItem.tsx
+++ components/pokemon/PokeCardItem.tsx
@@ -1,30 +1,40 @@
 import React, { FC } from 'react'
 import { Card, Grid, Row, Text } from '@nextui-org/react'
 import { SmallPokemon } from '../../interfaces'
+import { useRouter } from 'next/router'
 
-interface Props{
-    pokemon:SmallPokemon
+interface Props {
+    pokemon: SmallPokemon
 }
 
-export const PokeCardItem:FC<Props> = ({pokemon}) => {
-    const {id, name, img}= pokemon;
-  return (
-      <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-          <Card hoverable clickable>
-              <Card.Body>
-                  <Card.Image
-                      src={img}
-                      width="100%"
-                      height={140}
-                  />
-              </Card.Body>
-              <Card.Footer>
-                  <Row justify="space-between">
-                      <Text transform="capitalize">{name}</Text>
-                      <Text>#{id}</Text>
-                  </Row>
-              </Card.Footer>
-          </Card>
-      </Grid>
-  )
+export const PokeCardItem: FC<Props> = ({ pokemon }) => {
+    const { id, name, img } = pokemon;
+
+    const router = useRouter();
+    const handleClick = () => {
+        router.push(`/pokemon/${id}`);
+    }
+
+    return (
+        <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+            <Card
+                onClick={handleClick}
+                hoverable
+                clickable>
+                <Card.Body>
+                    <Card.Image
+                        src={img}
+                        width="100%"
+                        height={140}
+                    />
+                </Card.Body>
+                <Card.Footer>
+                    <Row justify="space-between">
+                        <Text transform="capitalize">{name}</Text>
+                        <Text>#{id}</Text>
+                    </Row>
+                </Card.Footer>
+            </Card>
+        </Grid>
+    )
 }
